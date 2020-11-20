61e28e161cfa6efe2ddc5368d93f00d3d779bb21
Kapil6 <kapilaggarwal100@gmail.com>
2020-11-20 23:58:26 +0530

    Added new folder which has algos based on the graph.

---
+++ Graph/graph_algos/prims_algo_for_mst.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <utility>
+
+using namespace std;
+const int MAX = 1e4 + 5;
+typedef pair<long long, int> PII;
+bool marked[MAX];
+vector <PII> adj[MAX];
+
+long long prim(int x)
+{
+    priority_queue<PII, vector<PII>, greater<PII> > Q;
+    int y;
+    long long minimumCost = 0;
+    PII p;
+    Q.push(make_pair(0, x));
+    while(!Q.empty())
+    {
+		cout << "In while loop" << endl;
+        // Select the edge with minimum weight
+        p = Q.top();
+        Q.pop();
+        x = p.second;
+        // Checking for cycle
+        if(marked[x] == true)
+            continue;
+        minimumCost += p.first;
+        marked[x] = true;
+        for(int i = 0;i < adj[x].size();++i)
+        {
+            y = adj[x][i].second;
+			cout << "x : " << x << " y : " << y << endl;
+            if(marked[y] == false)
+                Q.push(adj[x][i]);
+        }
+    }
+    return minimumCost;
+}
+
+int main()
+{
+    int nodes, edges, x, y;
+    long long weight, minimumCost;
+	cout << "Enter the no. of nodes and edges" << endl;
+    cin >> nodes >> edges;
+    for(int i = 0;i < edges;++i)
+    {
+		cout << "Enter the u v w" << endl;
+        cin >> x >> y >> weight;
+        adj[x].push_back(make_pair(weight, y));
+        adj[y].push_back(make_pair(weight, x));
+    }
+    // Selecting 1 as the starting node
+    minimumCost = prim(1);
+    cout << minimumCost << endl;
+    return 0;
+}
